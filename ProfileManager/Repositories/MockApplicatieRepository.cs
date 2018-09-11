5e3cc4334e25aeb6b8f978b4845b64bfdf6201d6
Gaston Rommens <gaston.rommens@gmail.com>
2018-09-11 22:46:56 +0200

    Full reorganisation to be able to use VS 2017

---
+++ ProfileManager/Repositories/MockApplicatieRepository.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProfileManager.Models;
+
+namespace ProfileManager.Repositories
+{
+    public class MockApplicatieRepository : IApplicatieRepository
+    {
+        private List<Applicatie> _applicaties;
+        public MockApplicatieRepository()
+        {
+            if (_applicaties == null)
+            {
+                InitializeApplicaties();
+            }
+        }
+
+        private void InitializeApplicaties()
+        {
+            _applicaties = new List<Applicatie>();
+            _applicaties.Add(new Applicatie { ApplicatieId = 1, Naam = "MijnZV", Versie = "1.0.0.164", Technologie = "ASP.NET" } );
+            _applicaties.Add(new Applicatie { ApplicatieId = 3, Naam = "WACViewer", Versie = "1.0.0.12", Technologie = "ASP.NET" } );
+            _applicaties.Add(new Applicatie { ApplicatieId = 4, Naam = "WACService", Versie = "1.0.0.14", Technologie = "WCF"} );
+            _applicaties.Add(new Applicatie { ApplicatieId = 5, Naam = "WACSSA", Versie = "1.0.0.81", Technologie = "ASP.NET" } );
+            _applicaties.Add(new Applicatie { ApplicatieId = 6, Naam = "Nota APP", Versie = "1.0.0.1", Technologie = "Outsystems" } );
+        }
+
+        public IList<Applicatie> GetAllApplicaties()
+        {
+            return _applicaties;
+        }
+
+        public Applicatie GetApplicatieById(int applicatieId)
+        {
+                return _applicaties.FirstOrDefault(a => a.ApplicatieId == applicatieId);
+        }
+    }
+}
